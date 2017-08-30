ae6fc2e4e58a2924270b229f2f28d17eeefa0331
Timo Schwämmlein <info@timoschwaemmlein.com>
2017-08-30 15:13:16 +0200

    add to Secret Number: smaller/bigger

--- 7.3 Guess the secret number.py
+++ 7.3 Guess the secret number.py
@@ -1,7 +1,16 @@
-secret = 7
-guess = int(raw_input("Please guess the secret number: "))
 
-if guess == secret:
-    print "That's right! You're a psychic from 7th heaven."
-else:
-    print "Eeh! Wrong. Sorry."
+
+secret = 7
+guess = 0
+
+while True:
+    guess = int(raw_input("Please guess the secret number: "))
+
+    if guess == secret:
+        print "That's right! You're a psychic from 7th heaven."
+        break
+    elif guess > secret:
+        print "That's wrong. It's the smaller things in life that matter."
+    elif guess < secret:
+        print "That's wrong. Go bigger or go home!"
+
